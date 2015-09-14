8561325e92a9e9a82855bae167a854bbffdd4635
Stefan Rumzucker <rumzucker@gmail.com>
2015-09-14 15:36:43 +0200

    Use Natron ...

---
+++ build/natronfile.js
@@ -0,0 +1,24 @@
+/*
+ * promote build
+ */
+import {resolve} from "path";
+import {task} from "natron";
+import {src, dest} from "vinyl-fs";
+
+const PKG_DIR = resolve(__dirname, "..");
+
+function builder(babelOpts: object, out: string) {
+  return function () {
+    let babel = require("gulp-babel");
+    return (src(resolve(PKG_DIR, "src", "**/*.js"))
+      .pipe(babel(babelOpts))
+      .pipe(dest(resolve(PKG_DIR, "dist", out)))
+    );
+  };
+}
+
+export var build = task.set([
+  builder({modules: "amd"}, "amd"),
+  builder({modules: "common"}, "cjs"),
+  builder({blacklist: ["es6"]}, "es6"),
+]);
